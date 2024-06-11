cc8b8fe3575ca2f5d2437e66cf231d0437c5f181
Mauroloon <mauricio.quezadagarcia@gmail.com>
2024-06-11 20:18:28 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,5 @@
+// Create web server
+// Create a web server that listens to port 3000 and serves the comments.json file.
+// The comments.json file will be sent to the client when the client sends a GET request to the server.
+// The comments.json file should be read from the disk and sent to the client with the correct content type.
+// The server should handle errors when the file is not found.
